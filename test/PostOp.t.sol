2f28bb20f9a0aa8f229cc2cccb17cb30b42f72de
Wilson Cusack <wilson.cusack@coinbase.com>
2024-03-11 13:27:27 -0400

    fix tests

--- test/PostOp.t.sol
+++ test/PostOp.t.sol
@@ -11,45 +11,29 @@ contract PostOpTest is PaymasterMagicSpendBaseTest {
 
     function test_transfersExcess(uint256 mode, uint256 amount_, uint256 maxCost_, uint256 actualCost) public {
         mode = bound(mode, 0, 1);
-        amount_ = bound(amount_, 0, address(magic).balance);
         maxCost_ = bound(maxCost_, 0, amount_);
         actualCost = bound(actualCost, 0, maxCost_);
+        amount = amount_;
 
         assertEq(withdrawer.balance, 0);
+        vm.deal(address(magic), amount);
         (bytes memory context,) = magic.validatePaymasterUserOp(_getUserOp(), userOpHash, maxCost_);
         uint256 expectedBalance = amount - actualCost;
-        vm.deal(address(magic), expectedBalance);
         magic.postOp(IPaymaster.PostOpMode(mode), context, actualCost);
         assertEq(withdrawer.balance, expectedBalance);
     }
 
-    // function test_DoesNotTransferIfPostOpFailed(uint256 amount_, uint256 maxCost_, uint256 actualCost) public {
-    //     amount_ = bound(amount_, 0, address(magic).balance);
-    //     maxCost_ = bound(maxCost_, 0, amount_);
-    //     actualCost = bound(actualCost, 0, maxCost_);
+    function test_RevertsIfPostOpFailed(uint256 amount_, uint256 maxCost_, uint256 actualCost) public {
+        maxCost_ = bound(maxCost_, 0, amount_);
+        actualCost = bound(actualCost, 0, maxCost_);
+        amount = amount_;
 
-    //     assertEq(withdrawer.balance, 0);
-    //     (bytes memory context,) = magic.validatePaymasterUserOp(_getUserOp(), userOpHash, maxCost_);
-    //     uint256 expectedBalance = 0;
-    //     magic.postOp(IPaymaster.PostOpMode.postOpReverted, context, actualCost);
-    //     assertEq(withdrawer.balance, expectedBalance);
-    // }
-
-    //     function test_PersistsExcessIfPostOpFailed(uint256 amount_, uint256 maxCost_, uint256 actualCost) public {
-    //         vm.assume(maxCost_ <= amount_);
-    //         vm.assume(actualCost <= maxCost_);
-    //         amount = amount_;
-    //         (bytes memory context,) = magic.validatePaymasterUserOp(_getUserOp(), userOpHash, maxCost_);
-    //         magic.postOp(IPaymaster.PostOpMode.postOpReverted, context, actualCost);
-    //         uint256 expectedBalance = amount - actualCost;
-    //         assertEq(withdrawer.balance, 0);
-    //         assertEq(magic.fundsExcessBalance(withdrawer), expectedBalance);
-    //         vm.stopPrank();
-    //         vm.deal(address(magic), expectedBalance);
-    //         if (expectedBalance > 0) {
-    //             vm.prank(withdrawer);
-    //             magic.withdrawGasExcess();
-    //             assertEq(withdrawer.balance, expectedBalance);
-    //         }
-    //     }
+        assertEq(withdrawer.balance, 0);
+        vm.deal(address(magic), amount);
+        (bytes memory context,) = magic.validatePaymasterUserOp(_getUserOp(), userOpHash, maxCost_);
+        uint256 expectedBalance = 0;
+        vm.expectRevert();
+        magic.postOp(IPaymaster.PostOpMode.postOpReverted, context, actualCost);
+        assertEq(withdrawer.balance, expectedBalance);
+    }
 }
