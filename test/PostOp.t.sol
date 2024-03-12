5bf164bd533219ede483294ed8c80e29eb51264a
Baptiste Oueriagli <baptiste.oueriagli@coinbase.com>
2024-03-12 17:58:21 +0000

    chore: force-transfer or revert in postOp

--- test/PostOp.t.sol
+++ test/PostOp.t.sol
@@ -22,32 +22,19 @@ contract PostOpTest is PaymasterMagicSpendBaseTest {
         assertEq(withdrawer.balance, expectedBalance);
     }
 
-    function test_DoesNotTransferIfPostOpFailed(uint256 amount_, uint256 maxCost_, uint256 actualCost) public {
+    function testRevert_PostOp_WhenCalledWithPostOpRevertedMode(uint256 amount_, uint256 maxCost_, uint256 actualCost)
+        public
+    {
         vm.assume(maxCost_ <= amount_);
         vm.assume(actualCost <= maxCost_);
         amount = amount_;
         assertEq(withdrawer.balance, 0);
         (bytes memory context,) = magic.validatePaymasterUserOp(_getUserOp(), userOpHash, maxCost_);
         uint256 expectedBalance = 0;
-        magic.postOp(IPaymaster.PostOpMode.postOpReverted, context, actualCost);
-        assertEq(withdrawer.balance, expectedBalance);
-    }
 
-    function test_PersistsExcessIfPostOpFailed(uint256 amount_, uint256 maxCost_, uint256 actualCost) public {
-        vm.assume(maxCost_ <= amount_);
-        vm.assume(actualCost <= maxCost_);
-        amount = amount_;
-        (bytes memory context,) = magic.validatePaymasterUserOp(_getUserOp(), userOpHash, maxCost_);
+        vm.expectRevert(MagicSpend.UnexpectedPostOpRevertedMode.selector);
         magic.postOp(IPaymaster.PostOpMode.postOpReverted, context, actualCost);
-        uint256 expectedBalance = amount - actualCost;
-        assertEq(withdrawer.balance, 0);
-        assertEq(magic.withdrawableFunds(withdrawer), expectedBalance);
-        vm.stopPrank();
-        vm.deal(address(magic), expectedBalance);
-        if (expectedBalance > 0) {
-            vm.prank(withdrawer);
-            magic.withdrawGasExcess();
-            assertEq(withdrawer.balance, expectedBalance);
-        }
+
+        assertEq(withdrawer.balance, expectedBalance);
     }
 }
