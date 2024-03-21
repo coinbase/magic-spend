2662df1ea468168f3f6bee430810b9a98b59edbd
katzman <steve.katzman@coinbase.com>
2024-03-21 12:33:46 -0700

    Get MagicSpend test coverage to 100%  (#14)
    * Add OwnerWithdraw test cases
    
    * Add test file for GetHash
    
    * Add test file for IsValidWithdrawalSignature
    
    * Add test for InsufficientBalance revert case
    
    * Rename getHash.t.sol to GetHash.t.sol
    
    * Remove commented lines
    
    * Format
    
    * Added coverage and coveralls to CI
    
    * Remove unecessary filter to lcov

---
+++ test/GetHash.t.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.21;
+
+import {MagicSpendTest} from "./MagicSpend.t.sol";
+import {MagicSpend} from "../src/MagicSpend.sol";
+import {MockERC20} from "solady/test/utils/mocks/MockERC20.sol";
+import {SignatureCheckerLib} from "solady/src/utils/SignatureCheckerLib.sol";
+
+contract GetHashTest is MagicSpendTest {
+    MockERC20 token = new MockERC20("test", "TEST", 18);
+
+    function test_returnsValidHash() public {
+        asset = address(token);
+        MagicSpend.WithdrawRequest memory request = _getRequest();
+        bytes32 expectedHash = SignatureCheckerLib.toEthSignedMessageHash(
+            abi.encode(
+                address(magic), withdrawer, block.chainid, address(token), request.amount, request.nonce, request.expiry
+            )
+        );
+        bytes32 testHash = magic.getHash(withdrawer, request);
+        assertEq(testHash, expectedHash);
+    }
+}
---
+++ test/IsValidWithdrawSignature.t.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.21;
+
+import "./MagicSpend.t.sol";
+import {MockERC20} from "solady/test/utils/mocks/MockERC20.sol";
+
+contract IsValidWithdrawalSignature is MagicSpendTest {
+    MockERC20 token = new MockERC20("test", "TEST", 18);
+
+    function test_returnsTrueWithValidSignature() public {
+        asset = address(token);
+        MagicSpend.WithdrawRequest memory request = _getRequest();
+        bool success = magic.isValidWithdrawSignature(withdrawer, request);
+        assert(success);
+    }
+
+    function test_returnsFalseWithInvalidSignature() public {
+        asset = address(token);
+        address invalidSender = address(0xdead);
+        MagicSpend.WithdrawRequest memory request = _getRequest();
+        bool success = magic.isValidWithdrawSignature(invalidSender, request);
+        assertFalse(success);
+    }
+}
